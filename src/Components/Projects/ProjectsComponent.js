338643ade1d052b1cb843c91df3600a1c9ff1556
Benjamin Proulx-Mathers <benjaminproulxmathers@gmail.com>
2020-12-06 23:12:03 -0500

    ADD: Work on projects components

--- src/Components/Projects/ProjectsComponent.js
+++ src/Components/Projects/ProjectsComponent.js
@@ -1,109 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
-
-const projects = [
-  {
-    title: 'Project Title 1',
-    subtitle: 'Project Subtitle 1',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 2',
-    subtitle: 'Project Subtitle 2',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 3',
-    subtitle: 'Project Subtitle 3',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 4',
-    subtitle: 'Project Subtitle 4',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 5',
-    subtitle: 'Project Subtitle 5',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 6',
-    subtitle: 'Project Subtitle 6',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 7',
-    subtitle: 'Project Subtitle 7',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 8',
-    subtitle: 'Project Subtitle 8',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 9',
-    subtitle: 'Project Subtitle 9',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 10',
-    subtitle: 'Project Subtitle 10',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 11',
-    subtitle: 'Project Subtitle 11',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 12',
-    subtitle: 'Project Subtitle 12',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 13',
-    subtitle: 'Project Subtitle 13',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 14',
-    subtitle: 'Project Subtitle 14',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 15',
-    subtitle: 'Project Subtitle 15',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 16',
-    subtitle: 'Project Subtitle 16',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 17',
-    subtitle: 'Project Subtitle 17',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 18',
-    subtitle: 'Project Subtitle 18',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 19',
-    subtitle: 'Project Subtitle 19',
-    year: '2020',
-  },
-  {
-    title: 'Project Title 20',
-    subtitle: 'Project Subtitle 20',
-    year: '2020',
-  },
-];
+import ProjectComponent from './ProjectComponent';
+import { projectsData } from './ProjectsData';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -120,26 +19,18 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '25px',
     color: theme.palette.accent.main,
   },
+  projectsListContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
   projectsList: {
     width: '100%',
     display: 'flex',
     flexWrap: 'wrap',
-  },
-  project: {
-    width: '33%',
-    minWidth: '300px',
-    height: '300px',
-    // border: '1px solid white',
-    display: 'flex',
-    flexDirection: 'column',
+    // alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center',
-  },
-  separator: {
-    backgroundColor: theme.palette.common.red,
-    height: '3px',
-    width: '30px',
-    margin: '10px 0',
+    // position: 'relative',
   },
 }));
 
@@ -151,17 +42,12 @@ const ProjectsComponent = () => {
       <div className={classes.sectionTitle}>
         <Typography variant={'h1'}>CREATIONS</Typography>
       </div>
-      <div className={classes.projectsList}>
-        {projects.map((project, index) => {
-          return (
-            <div key={index} className={classes.project}>
-              <Typography variant={'h4'}>{project.title}</Typography>
-              <Typography>{project.subtitle}</Typography>
-              <div className={classes.separator}></div>
-              <Typography variant={'h4'}>{project.year}</Typography>
-            </div>
-          );
-        })}
+      <div className={classes.projectsListContainer}>
+        <div className={classes.projectsList}>
+          {projectsData.map((project, index) => {
+            return <ProjectComponent key={index} project={project} />;
+          })}
+        </div>
       </div>
     </div>
   );
